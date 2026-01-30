3227d1ebac1f921b3ca075670da04bacda6519db
mythilisn <mythilisn15@gmail.com>
2026-01-30 15:33:08 +0530

    practiced binary operators

---
+++ practice/binary_operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 3;
+    int b = 10;
+    cout << "+ " << (a+b) << endl;
+    cout << "- " << (a-b) << endl;
+    cout << "* " << (a*b) << endl;
+    cout << "/ " << (a/b) << endl;
+    cout << "% " << (a%b) << endl;
+}
