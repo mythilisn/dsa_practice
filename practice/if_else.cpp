7a972323a59a58add630644b6f2cfaf921e6e74c
mythilisn <mythilisn15@gmail.com>
2026-02-01 22:57:06 +0530

    practiced conditional statemnts

---
+++ practice/if_else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "enter your age:";
+    cin >> age;
+    if (age>18) {
+        cout << "you can vote" << endl;
+    }
+    else
+        cout << "you can't vote" <<endl;
+     
+    }
