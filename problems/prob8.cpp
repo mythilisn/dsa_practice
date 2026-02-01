3151c607a47a5088c05db14d43c89defa1f2efc9
mythilisn <mythilisn15@gmail.com>
2026-02-01 23:11:14 +0530

    solved

---
+++ problems/prob8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cout << "enter the three numbers in order a, b, c: " << endl;
+    cin >> a >> b >> c;
+    if ((a>b) && (a>c)) {
+        cout << a << " is the largest number" << endl;
+    }
+    else if ((b>a) && (b>c)) {
+        cout << b << " is the largest number" << endl;
+    }
+    else {
+        cout << c << " is the largest number" << endl;
+    }
+}
