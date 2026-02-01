1d4059bb418baa3f8ab8f390e65bf0f43c37a3b0
mythilisn <mythilisn15@gmail.com>
2026-02-01 22:58:42 +0530

    solved

---
+++ problems/prob5.cpp
@@ -0,0 +1,18 @@
+// largest of two numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter the two numbers a and b in order: " << endl;
+    cin>>a>>b;
+    if (a>=b) {
+        cout<<a<<" is the largest number"<<endl;
+    }
+    else {
+        cout<<b<<" is the largest number"<<endl;
+
+    }
+    }
+
