64fcead52cbb96860ec4642e984827d5f4715790
mythilisn <mythilisn15@gmail.com>
2026-02-04 17:15:14 +0530

    for loop q

---
+++ problems/prob16.cpp
@@ -0,0 +1,16 @@
+//print the pattern of 4 stars in 4 lines
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<=4; i++) {
+        cout << "****";
+        cout << endl;
+        
+
+        }
+        return 0;
+    }
+    
